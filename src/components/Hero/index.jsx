c35e8eb0498147663ccfb1f6bbaa932430fd0682
Errol Watson <ewatsonrei@gmail.com>
2023-01-08 15:56:34 -0500

    first commit

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Header1 = styled.h1`
+    color: 333;
+`;
+
+const Subtitle = styled.span`
+    color: 333;
+`;
+
+const Hero = () => {
+    return (
+        <>
+            <Header1>Elite Dialysis Services</Header1>
+        </>
+    )
+}
---
+++ src/components/layout.js
@@ -0,0 +1,62 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+import NavBar from "./Nav/NavBar"
+
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${props => (props.theme === "purple" ? "purple" : "white")};
+  }
+`
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <GlobalStyle theme="purple" />
+      <NavBar  />
+      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `var(--space-5)`,
+            fontSize: `var(--font-sm)`,
+          }}
+        >
+          © {new Date().getFullYear()} &middot; Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default Layout
