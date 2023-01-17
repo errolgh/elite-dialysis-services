1e682a9b775383b12143e9f567092e9bba2cc561
Errol Watson <ewatsonrei@gmail.com>
2023-01-17 18:04:27 -0500

    map update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+const GoogleMapWidget = styled.iframe`
+    border: none;
+    width: 600px;
+    height: 300px;
+`;
 
+const zoomLevel = 14;
+const mapSource = `https://maps.google.com/maps?q=55%20Manchester%20Avenue,%20May%20Pen,%20Jamaica&t=&z=${zoomLevel}&ie=UTF8&iwloc=&output=embed`
 
 export default function Footer(){
     return (
         <>
             <h2>is a footer</h2>
-            <iframe width="500" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=55%20Manchester%20Avenue,%20May%20Pen,%20Jamaica&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <GoogleMapWidget src={mapSource} frameborder="0" scrolling="no" marginheight="0" marginwidth="0" />
         </>
     )
 }
